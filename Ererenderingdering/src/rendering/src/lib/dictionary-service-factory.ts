427e9b9c0edbc6c6b886c39c924ef11b04e62141
Antonytm <antontishchenko@gmail.com>
2023-03-04 22:26:06 +0200

    Hardcode root item id for dicsitonary service

--- Ererenderingdering/src/rendering/src/lib/dictionary-service-factory.ts
+++ Ererenderingdering/src/rendering/src/lib/dictionary-service-factory.ts
@@ -13,6 +13,7 @@ export class DictionaryServiceFactory {
           endpoint: config.graphQLEndpoint,
           apiKey: config.sitecoreApiKey,
           siteName: config.jssAppName,
+          rootItemId: '{42C91FB5-635C-4CA5-8E06-80C237789BCF}',
           /*
             The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
             app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
